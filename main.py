3a1de03676c672a75696e8ae50d718e1b4e7b532
heapy <momonster1999@gmail.com>
2023-02-06 12:32:21 +0100

    Made SteamAutoJoin join only "OG" groups

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import json
 import os
 
 if __name__ == "__main__":
-    os.system('cls' if os.name == 'nt' else 'clear')
+    os.system("cls" if os.name == "nt" else "clear")
 
     config_file = open("./config.json")
     config = json.load(config_file)
--- steamautojoin.py
+++ steamautojoin.py
@@ -1,9 +1,9 @@
-from steamwebapi.api import ISteamUser
-from steam.webapi import WebAPI
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 import time
+import re
+import json
 
 class SteamAutoJoin():
     def __init__(self, config: dict):
@@ -11,29 +11,27 @@ class SteamAutoJoin():
         self.password = config["password"]
         self.api_key = config["api_key"]
 
-        self.userclient = ISteamUser(steam_api_key = self.api_key)
-        self.webclient = WebAPI(key = self.api_key)
-        # self.groups = self.userclient.get_user_group_list(ENTER_YOUR_STEAMID64_HERE)
-
         self.driver_options = Options()
         self.driver_options.add_experimental_option("detach", True)
         self.driver_options.add_experimental_option("excludeSwitches", ["enable-logging"])
         self.driver = webdriver.Chrome("./chromedriver/chromedriver.exe", chrome_options = self.driver_options)
 
+        self.words = [x.lower() for x in json.load(open("./words.txt"))]
+
     def Start(self):
         print("Starting SteamAutoJoin...")
 
         print("Logging in...")
-        self.Login()
+        self._login()
         print("Logged in.")
 
         gid = 1
 
         while True:
-            self.Join(gid)
+            self._join(gid)
             gid = gid + 1
 
-    def Login(self):
+    def _login(self):
         try:
             self.driver.get("https://steamcommunity.com/login/home/")
 
@@ -43,7 +41,7 @@ class SteamAutoJoin():
             self.driver.find_elements(By.CLASS_NAME, "newlogindialog_TextInput_2eKVn")[1].send_keys(self.password)
             self.driver.find_element(By.CLASS_NAME, "newlogindialog_SubmitButton_2QgFE").click()
 
-            time.sleep(1)
+            time.sleep(2)
 
             if self.driver.find_element(By.CLASS_NAME, "newlogindialog_PrimaryHeader_39uMK").text == "STEAM GUARD":
                 self.driver.find_element(By.CLASS_NAME, "newlogindialog_TextLink_1cnUQ").click()
@@ -56,19 +54,29 @@ class SteamAutoJoin():
                     steam_guard_input.send_keys(steam_guard[index])
 
                 time.sleep(2)
+
         except:
             print("Couldn't login, retrying...")
+            self._login()
 
-    def Join(self, id: int):
+    def _join(self, id: int):
         try:
-            # if id in self.groups["response"]["groups"]:
-            #     print(f"Already joined group {id}.")
-            #     return
-
             self.driver.get(f"https://steamcommunity.com/gid/{id}")
+
+            clantag = self.driver.find_element(By.CLASS_NAME, "grouppage_header_abbrev")
+
+            if not self._check_clantag(clantag.text.lower()):
+                return
+
             self.driver.find_element(By.CLASS_NAME, "btn_green_white_innerfade").click()
 
-            time.sleep(0.5)
+            print(f"Joined OG group with id {id}.")
 
         except:
-            print(f"Couldn't join group with id {id}.")
+            pass
+
+    def _check_clantag(self, clantag: str, is_match = re.compile(r"[^a-z0-9]").search) -> bool:
+        if (len(clantag) < 4 and not bool(is_match(clantag))) or clantag in self.words:
+            return True
+
+        return False
